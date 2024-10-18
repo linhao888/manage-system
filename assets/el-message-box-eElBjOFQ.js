9b69c146b4445649a1a21f8b956430a369dffd65
Dai <2035721725@qq.com>
2024-10-19 02:26:58 +0800

    submit gh-pages1.1

--- assets/el-message-box-eElBjOFQ.js
+++
@@ -1,9 +0,0 @@
-import { aB as c } from './index-BvD4IwVX.js'
-const e =
-  (...a) =>
-  (s) => {
-    a.forEach((o) => {
-      c(o) ? o(s) : (o.value = s)
-    })
-  }
-export { e as c }
---
+++ assets/el-message-box-gjnOy4FP.js
@@ -0,0 +1,1 @@
+import{aB as c}from"./index-C_U30Pj1.js";const e=(...a)=>s=>{a.forEach(o=>{c(o)?o(s):o.value=s})};export{e as c};
